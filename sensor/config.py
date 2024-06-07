5ffe2787604cd269a4947cb1fda51f47f4ec26b5
Amartya vishwakarma <Amartya1820@gmail.com>
2024-06-07 06:14:36 +0530

    made 7 folders

--- sensor/config.py
+++ sensor/config.py
@@ -9,7 +9,5 @@ class EnvironmentVariable:
     mongo_db_url: str = os.getenv("MONGO_DB_URL")
    
 
-
-    
 env_vars = EnvironmentVariable()
 mongo_db_client = pymongo.MongoClient(env_vars.mongo_db_url)
